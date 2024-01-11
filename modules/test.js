64fd8ff4069b0ee7a57bf0eca97e0793ac20f313
Rorofer <115776169+Rorofer@users.noreply.github.com>
2024-01-11 16:55:04 +0100

    Ajout pages

---
+++ modules/test.js
@@ -0,0 +1,9 @@
+module.exports = {
+  A: function(res){
+    res.render('register.ejs');
+  },
+
+  test: function(){
+    console.log("Hello World");
+  }
+};
--- server.js
+++ server.js
@@ -1,14 +1,32 @@
+/*const a = require('./modules/test.js');*/
+
 const express = require('express');//Import d'Express, simplifie la gestion du backend
 const morgan = require('morgan');//Import de morgan, permet de log les connexions au serveur
 const ejs = require('ejs');//Import d'ejs : permet de rendre les .ejs, les <% %>
 const bodyParser = require('body-parser');// Import de body-parser : permet de récupérer les infos des formulaires sur le site
+const mysql = require('mysql');//Import de MySql, permettra de faire des requêtes vers la BDD
+const bcrypt = require('bcrypt');//Import de bcrypt, permet de chiffrer les MDP
 
 
 var app = express();//Création de l'app Express
 var server = require("http").createServer(app);//Crée le serveur
+var connection = mysql.createConnection({//Informations de connexion à la BDD
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'deuscord'
+});
+const saltRounds = 15;//Nombre de fois qu'il faut hash le MDP ( bcrypt )
 
 app.use(morgan('combined'));//Démarre les logs
 app.use(bodyParser.urlencoded({extended: true}));
+
+try{
+  connection.connect();//Connexion à la BDD
+}catch(error){
+  console.log("Impossible d'accéder à la BDD !!");
+  console.log(error);
+}
 
 app.use(function(req, res, next){
 //Fonction éxécutée à chaque chargement de page
@@ -28,6 +46,13 @@ app.post('/login/post', function(req, res){
   res.redirect('/login');
 });
 
+app.get('/register', function(req, res){
+  res.render('register.ejs')
+});
+app.get('/register/post', function(req, res){
+  res.render('register.ejs')
+});
+
 app.get('/style', function(req, res){
   res.render('style.ejs');
 });
@@ -39,4 +64,6 @@ app.get('/:url', function(req, res){//Même chose, mais la "route" est dynamique
   res.render('hello.ejs',{url: req.params.url});
 });
 
-app.listen(80, '0.0.0.0');//Le serveur démarre sur le port 80 ( HTTP par défaut en 80, HTTPS en 443), et écoute les connexions de toutes les IPs
+
+console.log("Serveur démarré !");
+app.listen(8080, '0.0.0.0');//Le serveur démarre sur le port 80 ( HTTP par défaut en 80, HTTPS en 443), et écoute les connexions de toutes les IPs
